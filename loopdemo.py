f955453d808808383f8be32045a23ad81be77490
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-14 08:21:05 -0500

    Updated from Brython Server: 2/14/2018 8:21:04 AM

--- loopdemo.py
+++ loopdemo.py
@@ -1,1 +1,26 @@
+#Maia Reynolds
+#2/14/18
+#loopdemo.py - using for and while loops
 
+""" #triple quotes comments out part
+#I love computer science 5 times
+for i in range(1,6): #only runs 5 times, doesn't do last # , could do 2 and 7, etc.
+    print("I <3 Computer Science")
+"""
+"""
+#Counts from 1 to ten
+for i in range(1,11):
+    print(i)
+"""
+"""
+#Count from 27 to 43 by 2s
+for i in range(27,44,2): #last # is by what number...
+    print(i)
+"""
+"""
+#add up numbers from 1 to 5
+total=0
+for i in range(1,6):
+    total=total+i
+print(total)
+"""
