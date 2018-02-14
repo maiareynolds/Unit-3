d2b6519aedffe765601dabcdcbed971de6ea4510
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-14 08:38:51 -0500

    Updated from Brython Server: 2/14/2018 8:38:50 AM

--- fives.py
+++ fives.py
@@ -1,1 +1,11 @@
+#Maia Reynolds
+#2/14/18
+#fives.py - prints out multiples of five up to number
 
+number=int(input("Enter a number: "))
+if number%5==0:
+    number=number+1
+else:
+    number=number
+for i in range(0,number,5):
+    print(i)
