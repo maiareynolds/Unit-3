82b4d10800d4e8e3e3a171107418482dd8d2fc25
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-28 08:34:14 -0500

    Updated from Brython Server: 2/28/2018 8:34:13 AM

--- warmUp8.py
+++ warmUp8.py
@@ -2,9 +2,9 @@
 #2/28/18
 #warmUp8.py - sum of numbers divisible by numbers
 
-number=0
+number=17
 sum=0
-for number in range(17,1001):
+for number in range(17,100001):
     if number%17==0 and number%3==0 and number%10==0:
         sum+=number
         number+=17
