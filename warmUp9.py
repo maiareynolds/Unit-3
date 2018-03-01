ec433d943afe602ee611b58bee3414d194f2604f
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-01 08:28:03 -0500

    Updated from Brython Server: 3/1/2018 8:28:02 AM

--- warmUp9.py
+++ warmUp9.py
@@ -1,1 +1,11 @@
+#Maia Reynolds
+#3/1/18
+#warmUp8.py - capitalize vowels
 
+text=input("Enter text: ")
+
+for ch in text:
+    if ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u":
+        print(ch.upper())
+    else:
+        print(ch)
