6a4bb692ff9b1be0b4cde97d4c48570786fa5f3a
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-01 08:09:58 -0500

    Updated from Brython Server: 3/1/2018 8:09:57 AM

--- dotsDemo.py
+++ dotsDemo.py
@@ -1,1 +1,12 @@
+#Maia Reynolds
+#3/1/18
+#dotsDemo.py - dots on screen
 
+from ggame import *
+blue=Color(0x6495ED,1)
+dot=CircleAsset(5,LineStyle(1,blue),blue)
+
+for i in range(10):
+    Sprite(dot,(25+25*i,25))
+
+App().run()
