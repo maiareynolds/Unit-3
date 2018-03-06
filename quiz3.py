55fb3952edb0d2b7dd2d17e3a9ef4995f5fbb10d
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-06 08:22:33 -0500

    Updated from Brython Server: 3/6/2018 8:22:33 AM

--- quiz3.py
+++ quiz3.py
@@ -1,3 +1,26 @@
 #Maia Reynolds
 #3/6/18
 #quiz.3.py
+
+#part 1
+num=-15
+while num<=-9:
+    print(num)
+    num+=1
+
+#part 2
+for i in range(50,17,-2):
+    print(i)
+
+#part 3
+num2=-100
+sum=0
+while num2<=1000:
+    sum=sum+num2
+    num2+=2
+print(sum)
+
+#part 4
+text=input("Enter text: ")
+while "alpaca" not in text:
+    text=input("Enter text: ")
