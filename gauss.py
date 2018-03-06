1bafa9e17fd0ce7db7ce607fd9fd32276c95d0b0
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-05 23:50:56 -0500

    Updated from Brython Server: 3/5/2018 11:50:55 PM

--- gauss.py
+++ gauss.py
@@ -1,1 +1,13 @@
+#Maia Reynolds
+#3/5/18
+#gauss.py - sum 1-100
 
+i=int(input("Enter a number: "))
+i2=int(input("Enter the second number: "))
+d=int(input("Enter the difference: "))
+sum=0
+
+while i<=i2:
+    sum=sum+i
+    i+=d
+print(sum)
